61fd9f8a59dcd79bb83c8465c6e2648f891f7198
Mario <mario98gp@gmail.com>
2024-01-26 13:08:37 +0100

    Commit inicial tras Spring Initializr

---
+++ src/main/java/es/mario/garcia/springboot/consultaRest/ConsultaRestApplication.java
@@ -0,0 +1,13 @@
+package es.mario.garcia.springboot.consultaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mario/garcia/springboot/consultaRest/ConsultaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mario.garcia.springboot.consultaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
